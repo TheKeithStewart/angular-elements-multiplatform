ca7e79399deb143ba416fdc885aea39c6421c369
Keith Stewart <kgstewart@gmail.com>
2018-06-19 20:04:32 -0500

    remove unnecessary properties from component

--- projects/header-element/src/app/header/header.component.ts
+++ projects/header-element/src/app/header/header.component.ts
@@ -1,10 +1,8 @@
 import { Component, ViewEncapsulation, Input } from '@angular/core';
 
 @Component({
-  selector: 'header-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  encapsulation: ViewEncapsulation.Native
 })
 export class HeaderComponent {
   @Input() title = 'Default Title';
